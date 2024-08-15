d1f50bb666469d6ae7147e8bc97f1b63050d0d70
Annapirz <annapirz@MacBook-Air-Annapirz.local>
2024-08-15 18:00:09 +0300

    feat: components app

--- bro-pizza/src/App.js
+++ bro-pizza/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import Header from './components/header';
+import Categories from './components/Categories';
+import Sort from './components/sort';
+import Pizzablock from './components/pizzablock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+          <Pizzablock title='Мексиканская пицца' price={350} />
+          <Pizzablock title='Домашняя пицца' price={450} />
+          <Pizzablock title='Обычная пицца' price={300}/>
+          <Pizzablock title='Необычная пицца' price={301}/>
+          <Pizzablock title='Дьявольская пицца' price={666}/>
+          <Pizzablock title='Пицца Айтишник' price={404}/>
+          <Pizzablock title='Голубая пицца ' price={609}/>
+          <Pizzablock title='Гавайская пицца ' price={470}/>
+          <Pizzablock title='Ананасовая пицца' price={520}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
